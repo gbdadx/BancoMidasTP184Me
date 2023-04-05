87924e1c423a524186ab74f11f781bc408fbd3df
gabriela diaz <gbdadx@gmail.com>
2023-04-05 10:40:46 -0300

    changes to bot and scroll up button

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,9 @@
+
 // Get the button:
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
