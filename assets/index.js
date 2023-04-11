287fd5f29e107abbeefe3e51ee6c7f93d3c55ca4
gabriela diaz <gbdadx@gmail.com>
2023-04-10 21:26:28 -0300

    traslated some comments

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,8 @@
 
-// Get the button:
+// consigue el boton
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// cuando el usuario desciende 300px desde el top de la pagina, aparece el boton de scroll
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
@@ -13,8 +13,8 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// cuando el usuario clickea el boton, sube al top de la pagina
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // para safari
+  document.documentElement.scrollTop = 0; // oara  Chrome, Firefox, IE y Opera
 }
